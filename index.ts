149fc2b6a8f16fab6eb31d389c53c757b5eac411
BuildTools <Idevtanm@gmail.com>
2020-10-12 22:09:59 -0500

    T

--- index.ts
+++ index.ts
@@ -5,17 +5,17 @@ import fs from 'fs/promises';
  * @arg path Path to directory
  * @arg depth How deep to recurse into subdirectories
  */
-const importAll = async (path: string, depth = Infinity, _ = 0) => {
+const importAll = async <T>(path: string, depth = Infinity, _ = 0) => {
   const files = await fs.readdir(new URL(path), {
     withFileTypes: true
   });
 
-  const modules: unknown[] = [];
+  const modules: Promise<(T extends (infer I)[] ? I : T)[]>[] = [];
 
   for (const file of files) {
     const filePath = `${path}/${file.name}`;
     if (file.isDirectory() && depth > _) {
-      modules.push(importAll(filePath, depth, _ + 1));
+      modules.push(importAll<T>(filePath, depth, _ + 1));
     }
     else if (filePath.slice(-3) === '.js') {
       modules.push(import(filePath));
